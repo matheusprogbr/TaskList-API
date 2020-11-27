d279e2d62a3bde53d4a8832ee75c78e96dad7ea4
Matheus Henrique <matheusprogbr@gmail.com>
2020-11-26 21:23:47 -0300

    feat: Add session authentication and JWT generator

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd308f3d412778b2b91133b77d91b749d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import user from './app/controllers/user';
+import session from './app/controllers/session';
 
 const routes = express.Router();
 
 routes.get('/', user.index);
 routes.post('/users', user.store);
+
+routes.post('/sessions', session.store);
 
 export default routes;
